7c031ccd1a583a972d03ec8c7d4f02a5dfe99beb
Luiz Augusto <augustodia97@gmail.com>
2021-07-28 21:01:44 -0300

    Adicionando seção de Produtos

--- index.php
+++ index.php
@@ -1,9 +1,36 @@
-<?php include('./pages/header.php');?>
+<?php include('./pages/header.php'); ?>
 <header class="banner">
   <div class="container">
     <div class="banner-content">
-      <h1 class="banner-title">Desconto de até 80% em toda a loja. Aproveite já!</h1>
+      <h1 class="banner-title">Desconto de até 80% em todos os produtos. Aproveite já!</h1>
       <a class="btn" href="#quem_somos">SAIBA MAIS</a>
     </div>
   </div>
 </header>
+
+<div class="bg-produtos">
+  <img src="./images/bg-produtos.svg" alt="">
+</div>
+<section class="produtos container">
+  <h2 class="title">Produtos</h2>
+  <div class="produtos-content">
+    <div class="produto-item">
+      <img src="./images/pipoca-doce.png" alt="">
+      <h3 class="produto-titulo">Pipoca doce</h3>
+     <p class="preco"><span>R$</span> 12,90</p>
+      <a class="btn btn-comprar">SAIBA MAIS</a>
+    </div>
+    <div class="produto-item">
+      <img src="./images/pirulito.png" alt="">
+      <h3 class="produto-titulo">Pirulito</h3>
+      <p class="preco"><span>R$</span> 12,90</p>
+      <a class="btn btn-comprar">SAIBA MAIS</a>
+    </div>
+    <div class="produto-item">
+      <img src="./images/jujubas.png" alt="">
+      <h3 class="produto-titulo">Jujubas</h3>
+     <p class="preco"><span>R$</span> 12,90</p>
+      <a class="btn btn-comprar">SAIBA MAIS</a>
+    </div>
+  </div>
+</section>
