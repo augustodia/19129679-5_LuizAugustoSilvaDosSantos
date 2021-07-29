e1a97806c0472ac7cf710eda274c0490c6d51827
Luiz Augusto <augustodia97@gmail.com>
2021-07-28 21:11:18 -0300

    Separando a página produtos em uma Page específica

--- index.php
+++ index.php
@@ -8,29 +8,6 @@
   </div>
 </header>
 
-<div class="bg-produtos">
-  <img src="./images/bg-produtos.svg" alt="">
-</div>
-<section class="produtos container">
-  <h2 class="title">Produtos</h2>
-  <div class="produtos-content">
-    <div class="produto-item">
-      <img src="./images/pipoca-doce.png" alt="">
-      <h3 class="produto-titulo">Pipoca doce</h3>
-     <p class="preco"><span>R$</span> 12,90</p>
-      <a class="btn btn-comprar">SAIBA MAIS</a>
-    </div>
-    <div class="produto-item">
-      <img src="./images/pirulito.png" alt="">
-      <h3 class="produto-titulo">Pirulito</h3>
-      <p class="preco"><span>R$</span> 12,90</p>
-      <a class="btn btn-comprar">SAIBA MAIS</a>
-    </div>
-    <div class="produto-item">
-      <img src="./images/jujubas.png" alt="">
-      <h3 class="produto-titulo">Jujubas</h3>
-     <p class="preco"><span>R$</span> 12,90</p>
-      <a class="btn btn-comprar">SAIBA MAIS</a>
-    </div>
-  </div>
-</section>
+<?php include('./pages/produtos.php'); ?>
+
+
